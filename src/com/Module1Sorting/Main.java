9fd040f45148528ad7f62e776aeb2f80a0c02ca8
Kalachuchi <yuriefraim.casabuena@benilde.edu.ph>
2021-09-10 23:19:30 +0800

    Module 1 done

--- src/com/Module1Sorting/Main.java
+++ src/com/Module1Sorting/Main.java
@@ -3,35 +3,57 @@ package com.Module1Sorting;
 public class Main {
 
     public static void main(String[] args) {
-        int[] numbers = new int[10];
+        int[] bubbleNumbers = new int[10];
+        int[] selectionNumbers = new int[10];
 
-        numbers[0] = 10;
-        numbers[1] = 20;
-        numbers[2] = -10;
-        numbers[3] = 109;
-        numbers[4] = 1214;
-        numbers[5] = 256;
-        numbers[6] = 420;
-        numbers[7] = -69;
-        numbers[8] = 1337;
-        numbers[9] = 91021;
-        System.out.println("Before selection sort:");
-        printArrayElements(numbers);
-        selectionSort(numbers);
+        bubbleNumbers[0] = 16;
+        bubbleNumbers[1] = 65;
+        bubbleNumbers[2] = -10;
+        bubbleNumbers[3] = 1337;
+        bubbleNumbers[4] = 2016;
+        bubbleNumbers[5] = 322;
+        bubbleNumbers[6] = 420;
+        bubbleNumbers[7] = -55334;
+        bubbleNumbers[8] = 55432;
+        bubbleNumbers[9] = 91021;
 
-        System.out.println("\n\nAfter selection sort:");
-        printArrayElements(numbers);
+        selectionNumbers[0] = 16;
+        selectionNumbers[1] = 65;
+        selectionNumbers[2] = -10;
+        selectionNumbers[3] = 1337;
+        selectionNumbers[4] = 2016;
+        selectionNumbers[5] = 322;
+        selectionNumbers[6] = 420;
+        selectionNumbers[7] = -55334;
+        selectionNumbers[8] = 55432;
+        selectionNumbers[9] = 91021;
+
+        System.out.println("Before bubble sort:");
+        printArrayElements(bubbleNumbers);
+
+        bubbleSort(bubbleNumbers);
+
+        System.out.println("\n\nAfter bubble sort descending order:");
+        printArrayElements(bubbleNumbers);
+
+        System.out.println("\n\nBefore selection sort descending order:");
+        printArrayElements(selectionNumbers);
+
+        selectionSort(selectionNumbers);
+
+        System.out.println("\n\nAfter selection sort descending order:");
+        printArrayElements(selectionNumbers);
     }
     private static void bubbleSort(int[] arr) {
         for (int lastSortedIndex = arr.length - 1;lastSortedIndex > 0; lastSortedIndex-- )
         {
             for (int i = 0; i < lastSortedIndex; i++)
             {
-                if (arr[i] > arr[i + 1])
+                if (arr[i] < arr[i + 1])
                 {
-                    int temp = arr[i];
-                    arr[i] = arr[i + 1];
-                    arr[i + 1] = temp;
+                    int temp = arr[i + 1];
+                    arr[i + 1] = arr[i];
+                    arr[i] = temp;
                 }
             }
         }
@@ -40,25 +62,23 @@ public class Main {
     {
         for(int lastSortedIndex = arr.length - 1; lastSortedIndex > 0; lastSortedIndex--)
         {
-            int largestIndex = 0;
+            int shortestIndex = 0;
             for(int i = 1; i <= lastSortedIndex; i++)
             {
-                if(arr[i] > arr[largestIndex])
+                if(arr[i] < arr[shortestIndex])
                 {
-                    largestIndex = i;
+                    shortestIndex = i;
                 }
             }
-
             int temp = arr[lastSortedIndex];
-            arr[lastSortedIndex] = arr[largestIndex];
-            arr[largestIndex] = temp;
-
+            arr[lastSortedIndex] = arr[shortestIndex];
+            arr[shortestIndex] = temp;
         }
     }
     private static void printArrayElements(int[] arr)
     {
             for (int j : arr) {
-                System.out.println(j);
+                System.out.print(j + " ");
             }
         }
     }
